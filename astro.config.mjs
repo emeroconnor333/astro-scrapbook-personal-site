90a5d8f2a65c0e9e6a4b02641396505be5eaa555
Emer O Connor <emeroconnor333@gmail.com>
2026-05-10 21:26:06 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  output: "static",
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  image: {
+    defaultImageFormat: "webp",
+  },
+});
